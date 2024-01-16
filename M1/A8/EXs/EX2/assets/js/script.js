a393b8bebe92114bccf94b623e61d712d99e1d8d
Daniel Mendes <senseidanielmendes@gmail.com>
2024-01-16 20:50:43 -0300

    docs: add EX3 A8 M1

--- M1/A8/EXs/EX2/assets/js/script.js
+++ M1/A8/EXs/EX2/assets/js/script.js
@@ -1,18 +1,34 @@
-// CONDICIONAIS EM JAVASCRIPT
-var nome = "Dan";
+const numero1 = 20;
+const numero2 = 30;
+const numero3 = "30";
 
-if (nome == "Dan") {
-    console.log("Sim, você é Dan!");
-}
-else{
-    console.log("Não, você não é o Dan!");
-};
-// -------------------------------------
-var numBingo = 38;
+// == -> igual que 
 
-if (numBingo == 38) {
-    console.log("Parabéns!! Você ganhou no bingo!");
+// igual a (nome == usuario) false
+// maior que (numero1 > numero2) false
+// maior ou igual (numero1 >= numero2) false
+// menor que (numero1 < numero2) verdadeiro
+// menor ou igual (numero1 <= numero2) verdadeiro
+// igual a (numero2 == numero3) verdadeiro
+// estritamente igual (numero2 === numero3) falso
+// diferente de (numero2 != numero3) verdadeiro
+
+//abrir bloco de código {}
+// ESTRUTURA DE CONTROLE IF ELSE
+// se(tenho fogao e tenho panela){
+//   // o que vai acontecer caso seja verdadeira a condicao
+// }se nao{
+//   // o que vai acontecer caso seja falsa a condicao
+// }
+
+//verificar = if else
+
+const nome = "Beatriz"
+const usuario = "Dan"
+
+
+if(numero1 > numero2){
+    console.log("valor é maior")
+}else{
+    console.log("nao é maior ")
 }
-else {
-    console.log("Vixi.. tu perdeu, mano");
-};
