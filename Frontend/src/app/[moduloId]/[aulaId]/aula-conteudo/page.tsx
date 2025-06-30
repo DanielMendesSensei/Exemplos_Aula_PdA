ed34fb1de9b242531632e08c80e5a5f00928cd4a
DgDevHub <cavaleirooficia@gmail.com>
2025-06-30 17:45:45 -0300

    :bulb: docs: I commented what is still in dev

--- Frontend/src/app/[moduloId]/[aulaId]/aula-conteudo/page.tsx
+++ Frontend/src/app/[moduloId]/[aulaId]/aula-conteudo/page.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/Header/Header";
-import { getAulaById } from "@/lib/aulas"; -> Simulando puxando dados da aula
+// import { getAulaById } from "@/lib/aulas"; -> Simulando puxando dados da aula
 import { ParamsProps } from "@/types/types";
 import React from "react";
 
 export default async function AulaPage({ params }: ParamsProps) {
   const { aulaId } = await params;
-  const aula = await getAulaById(aulaId);
-  if (!aula) return <p> Aula não encontrada </p>;
+//   const aula = await getAulaById(aulaId);
+//   if (!aula) return <p> Aula não encontrada </p>;
 
   return (
     <>
@@ -14,9 +14,9 @@ export default async function AulaPage({ params }: ParamsProps) {
         <h1>Você está na {aulaId}</h1>
       </Header>
       <section>
-        <video controls src={aula.videoUrl} width="100%" />
-        <p>{aula.descricao}</p>
-        <p> Teste aqui </p>
+        {/* <video controls src={aula.videoUrl} width="100%" />
+        <p>{aula.descricao}</p> */}
+        <p> apague esse p </p>
       </section>
     </>
   );
